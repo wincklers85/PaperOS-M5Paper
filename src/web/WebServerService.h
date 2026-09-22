edcf64b190313a2ba189a438cce59eb6d522621d
wincklers85 <wincklers85@googlemail.com>
2026-09-22 18:15:31 +0200

    feat: add embedded Web server and REST API

---
+++ src/web/WebServerService.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <Arduino.h>
+#include <WebServer.h>
+#include <Update.h>
+#include "ConfigManager.h"
+#include "WiFiManager.h"
+#include "StorageManager.h"
+#include "NotesService.h"
+#include "PowerManager.h"
+#include "UiManager.h"
+
+namespace paperos {
+class WebServerService {
+ public:
+  WebServerService(ConfigManager& c, WiFiManager& w, StorageManager& s, NotesService& n, PowerManager& p, UiManager& u)
+  : config_(c), wifi_(w), storage_(s), notes_(n), power_(p), ui_(u), server_(80) {}
+  void begin();
+  void loop() { server_.handleClient(); }
+ private:
+  ConfigManager& config_; WiFiManager& wifi_; StorageManager& storage_; NotesService& notes_; PowerManager& power_; UiManager& ui_;
+  WebServer server_; String sessionToken_; File uploadFile_; bool otaOk_=false;
+  bool authorized();
+  void sendJson(int code,const String& json);
+  String body();
+  String newToken();
+  void routes();
+};
+}

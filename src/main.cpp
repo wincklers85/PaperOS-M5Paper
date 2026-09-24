4ae49067e5a1f75ec2b25d4194de746ce632fe09
wincklers85 <wincklers85@googlemail.com>
2026-09-24 17:15:24 +0200

    feat: initialize Phone Link and auto-import editable Wi-Fi SD file

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "network/WiFiManager.h"
 #include "services/NotesService.h"
 #include "services/PowerManager.h"
+#include "services/PhoneLinkService.h"
 #include "ui/UiManager.h"
 #include "web/WebServerService.h"
 
@@ -20,8 +21,9 @@ StorageManager storageManager;
 WiFiManager wifiManager(configManager);
 NotesService notesService(storageManager);
 PowerManager powerManager(configManager);
-UiManager uiManager(displayManager,touchManager,wifiManager,storageManager,powerManager,configManager);
-WebServerService webServer(configManager,wifiManager,storageManager,notesService,powerManager,uiManager);
+PhoneLinkService phoneLinkService;
+UiManager uiManager(displayManager,touchManager,wifiManager,storageManager,powerManager,configManager,phoneLinkService);
+WebServerService webServer(configManager,wifiManager,storageManager,notesService,powerManager,uiManager,phoneLinkService);
 Logger logger;
 
 void setup() {
@@ -32,6 +34,10 @@ void setup() {
 
   if (!configManager.begin()) Serial.println("[PaperOS] LittleFS/config init failed");
   bool sdOk = storageManager.begin();
+  if (sdOk) {
+    if (SD.exists("/PaperOS/Config/wifi_networks.txt")) configManager.importWifiTextFromSd();
+    else configManager.exportWifiTextToSd();
+  }
   logger.begin(sdOk ? &storageManager.fs() : nullptr);
   logger.info(String("Boot ") + NAME + " " + VERSION);
   if (!sdOk) logger.warn("microSD not mounted; SD-backed features disabled");

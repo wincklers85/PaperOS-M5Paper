f2d977a824fb58b690690311f9b99a41ec757265
wincklers85 <wincklers85@googlemail.com>
2026-09-24 17:03:22 +0200

    feat: add swipe gesture detection for PaperOS touch UI

--- src/drivers/TouchManager.h
+++ src/drivers/TouchManager.h
@@ -2,9 +2,40 @@
 #include <M5Unified.h>
 
 namespace paperos {
-struct TouchEvent { bool clicked = false; int x = 0; int y = 0; };
+
+enum class TouchGesture : uint8_t {
+  None = 0,
+  Tap,
+  SwipeUp,
+  SwipeDown,
+  SwipeLeft,
+  SwipeRight
+};
+
+struct TouchEvent {
+  bool active = false;
+  bool clicked = false;
+  bool released = false;
+  int x = 0;
+  int y = 0;
+  int startX = 0;
+  int startY = 0;
+  int dx = 0;
+  int dy = 0;
+  TouchGesture gesture = TouchGesture::None;
+};
+
 class TouchManager {
  public:
   TouchEvent poll();
+
+ private:
+  bool tracking_ = false;
+  int startX_ = 0;
+  int startY_ = 0;
+  int lastX_ = 0;
+  int lastY_ = 0;
+  uint32_t startedMs_ = 0;
 };
+
 }
